73d8f3bd78be64094ee6902027a90a6b42ee7d5a
mohammad kamruzzaman <romon.zaman@gmail.com>
2026-01-17 16:00:50 +0600

    redhat copy of centos

---
+++ redhat/resources/config.sh
@@ -0,0 +1,19 @@
+
+# FusionPBX Settings
+system_username=admin           # default username admin
+system_password=random          # random or as a pre-set value
+system_branch=master            # master, stable
+
+# FreeSWITCH Settings
+switch_branch=stable            # master, stable
+switch_source=false             # true or false
+switch_package=true             # true or false
+
+# Database Settings
+database_name=fusionpbx                     # Database name (safe characters A-Z, a-z, 0-9)
+database_username=fusionpbx                 # Database username (safe characters A-Z, a-z, 0-9)
+database_password=random                    # random or a custom value (safe characters A-Z, a-z, 0-9)
+database_repo=official                      # PostgreSQL official, system, 2ndquadrant
+database_host=127.0.0.1                     # hostname or IP address
+database_port=5432                          # port number
+database_backup=false                       # true or false
